75e5585c66f7e36b867e92373820347eb1573471
ninn <408763524@qq.com>
2019-11-07 18:46:31 +0800

    init

---
+++ Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+    
+    static {
+        String nativeExecuter = "/home/ninn/jnidemo/jni/Demo.so";
+        nativeExecuter = "/home/ninn/jnidemo/interface/lib/libOsiInterface.so";
+    	System.load(nativeExecuter);
+    }
+
+    public static void main(String[] args) {
+        //System.loadLibrary("Hello");
+        System.out.println("welcome");
+        sayHello();
+    }
+
+    private static native void sayHello();
+}
