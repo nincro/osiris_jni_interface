416cb3cd59e460f53d48ea6230d06aa89eb8c16b
ninn <408763524@qq.com>
2019-11-11 09:13:27 +0800

    add interface

--- JniImpl.java
+++ JniImpl.java
@@ -8,6 +8,10 @@ public class JniImpl {
 
     private static native void getCodeAndMask();
 
+    private static native String matching(String code_rdir, String mask_rdir);
+
+    private static native String getCodeAndMask(String img_rdir);
+
     public static void main(String[] args) {
         //System.loadLibrary("Hello");
         System.out.println("welcome");
