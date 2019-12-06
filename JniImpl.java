f4e13e787c0b06a018662aa93ed2eb62d76fe4e8
ninn <408763524@qq.com>
2019-12-06 14:59:12 +0800

    add matching interface

--- JniImpl.java
+++ JniImpl.java
@@ -8,14 +8,17 @@ public class JniImpl {
 
     
 
-    private static native String matching(String code_rdir, String mask_rdir);
+    //private static native String matching(String code_rdir, String mask_rdir);
 
     private static native String getCodeAndMask(String img_rdir);
+    
+    public static native String matching(String a_rdir, String b_rdir);
 
     public static void main(String[] args) {
         //System.loadLibrary("Hello");
-        System.out.println("welcome");
-       getCodeAndMask("0000_000.bmp");
+       System.out.println("welcome");
+       String code_rdir = getCodeAndMask("0000_000.bmp");
+       System.out.println(code_rdir);
     }
 
     private static native void sayHello();
