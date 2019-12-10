168fa629de8790468cf894796c80df17b23ce020
ninn <408763524@qq.com>
2019-12-10 21:01:29 +0800

    fix demo:

--- Demo.java
+++ Demo.java
@@ -6,7 +6,7 @@ public class Demo {
         //System.loadLibrary("Hello");
         System.out.println("jni demo");
         String rdir = JniImpl.getCodeAndMask("0000_000.bmp");
-        System.out.println(code_rdir);
+        System.out.println(rdir);
     }
 
 }
