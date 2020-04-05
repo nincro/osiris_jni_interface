1803016e98ae73430df47029ecebdb14a5ecb1dd
ninn <408763524@qq.com>
2020-04-05 16:59:17 +0800

    tested

--- interface_iris_detect/jni/JniInterface.java
+++ interface_iris_detect/jni/JniInterface.java
@@ -8,11 +8,15 @@ static {
 
         String nativeSharedLib = "/home/ninn/jnidemo/osiris_jni_interface/interface/lib/libOsiInterface.so";
         String rdir = "lib/libOsiInterface.so";
-        nativeSharedLib = Paths.get("interface").toAbsolutePath().resolve(rdir).toString();
+        nativeSharedLib = Paths.get(".").toAbsolutePath().resolve(rdir).toString();
         System.out.println("nativeSharedLib:"+nativeSharedLib);
         System.load(nativeSharedLib);
     }
 
 public static native String irisDetect(String rdir);
 
+public static void main(String[] args){
+String mask_rdir = irisDetect("0000_000.bmp");
+System.out.println(mask_rdir);
+}
 }
